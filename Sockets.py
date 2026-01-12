b1069a9d10602da304b5a15df72e13b64d147272
K4DD05 <shadiaddo377@gmail.com>
2026-01-12 12:50:48 +0000

    Fix variable name from target_ports to target_port

--- Sockets.py
+++ Sockets.py
@@ -1,12 +1,12 @@
 import socket
 #Below, you would have to input your target ip and the specific port
 target_ip = input(str("Enter web address or ip address: "))
-target_ports = int(input("Enter specific port: "))
+target_port = int(input("Enter specific port: "))
 
 MySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 MySocket.settimeout(1)
 #connect_ex returns an integer to show the success of the conection and only returns 0 when the connection is successful.
-Nettuple = (target_ip, target_ports)
+Nettuple = (target_ip, target_port)
 result = MySocket.connect_ex(Nettuple)
 
 if result == 0:
@@ -15,3 +15,4 @@ else:
     print ("NOT OPEN")
 
 MySocket.close
+
